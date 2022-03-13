1c354ae9f9483c7970d4770316e6b7ccf6254fda
Dmitry Borodachev <borodachev.58@mail.ru>
2022-03-13 12:00:00 +0300

    пофиксил проблему с библиотекой, теперь dto берутся из common module

--- core/src/main/java/liga/medical/messageanalyzer/core/RabbitMqListener.java
+++ core/src/main/java/liga/medical/messageanalyzer/core/RabbitMqListener.java
@@ -1,6 +1,6 @@
 package liga.medical.messageanalyzer.core;
 
-import dto.DeviceIdentificationDto;
+import liga.medical.DeviceIdentificationDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
